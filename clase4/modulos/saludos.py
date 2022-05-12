4ec327a84f2b42b1679e4fe659e580682af5a185
tavo <hola@tavo.dev>
2022-05-11 19:23:55 -0500

    se agregaron nuevos saludos

--- clase4/modulos/saludos.py
+++ clase4/modulos/saludos.py
@@ -2,5 +2,10 @@
 def saludo():
     print("Hola")
 
+def saludo_nombre(nombre):
+    print(f"hola {nombre}")
+
+def saludo_nombre_completo(nombre, apellido_paterno, apellido_materno=''):
+    print(f"Hola {nombre} {apellido_paterno} {apellido_materno}")
 
 nombre = "nombre"
