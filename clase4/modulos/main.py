ddc763c275ef00843593f3752b52608b5d862971
tavo <hola@tavo.dev>
2022-05-11 19:29:52 -0500

    ejemplo importacion global

--- clase4/modulos/main.py
+++ clase4/modulos/main.py
@@ -2,7 +2,13 @@
 #saludos.saludo()
 #print(saludos.nombre)
 
-from saludos import saludo_nombre
+#from saludos import saludo_nombre
+#
+#saludo_nombre("Lucio")
+#saludo_nombre("Pako")
 
+from saludos import *
+
+saludo()
 saludo_nombre("Lucio")
-saludo_nombre("Pako")
+saludo_nombre_completo("Luis", "Perez")
