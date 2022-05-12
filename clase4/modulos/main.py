40abf18dc45d100c79cf31a035f887f245c05015
tavo <hola@tavo.dev>
2022-05-11 19:32:58 -0500

    ejemplo de alias

--- clase4/modulos/main.py
+++ clase4/modulos/main.py
@@ -7,8 +7,17 @@
 #saludo_nombre("Lucio")
 #saludo_nombre("Pako")
 
-from saludos import *
+#from saludos import *
+#
+#saludo()
+#saludo_nombre("Lucio")
+#saludo_nombre_completo("Luis", "Perez")
 
-saludo()
-saludo_nombre("Lucio")
-saludo_nombre_completo("Luis", "Perez")
+# paquete saludos_miguel
+# paquete saludos_sarai
+
+# ejemplo de alias
+#from saludos_miguel import saludos as saludo_1
+#from saludos_sarai  import saludos as sadulo_2
+#
+#saludo_1
