f3f43acfb5c4381873c367f22e9c4005385aaf03
tavo <hola@tavo.dev>
2022-05-16 19:25:18 -0500

    instrucciones actividad monitorio con psutil

---
+++ clase5/monitor2.py
@@ -0,0 +1,4 @@
+# pasar todo lo del monitor 1 a un archivo de texto
+# crear una carpeta donde se almacenaran los logs 
+# crear un archivo con X nombre 
+# insertar la informacion recabada en el archivo
