c8b081028430ac1eb57342b2d995e99bcf1084cc
tavo <hola@tavo.dev>
2022-05-16 20:32:49 -0500

    actividad de excepciones y modulos

---
+++ clase5/exepciones3.py
@@ -0,0 +1,12 @@
+import os
+# Actividad excepciones
+# Replicar el comportamiento de creacion de carpetas con Windows
+
+# al crear un directorio si ya existe agregarle entre parentesis el consecutivo
+# carpeta
+# carpeta(1)
+# carpeta(2)
+
+contenido = os.listdir('C:\\Users\\tavodev\\Desktop\\python_intermedio_github')
+
+print(contenido)
