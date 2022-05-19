4388deac8eee3a18cfb1c2ba44aaa7b13118b88b
tavo <hola@tavo.dev>
2022-05-18 19:50:56 -0500

    practica de csv con varios modulos

--- clase6/csvfile2.py
+++ clase6/csvfile2.py
@@ -1,10 +1,39 @@
 import csv
+import os
+import time
+
+start = time.time()
+
+os.system('cls')
+ruta = "C:\\Users\\tavodev\\Desktop\\python_intermedio_github\\clase6\\practica\\"
 
 with open('console_games.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row['Name'], row['Platform'])
+        print(f"plataforma => {row['Platform']} año=> {row['Year']}")
+        
+        try:
+            if row['Platform']:
+                os.makedirs(f"{ruta}{row['Platform']}\\{row['Year']}")
 
+        except FileExistsError as ex:
+            pass
+
+        if row['Platform']:
+            ruta_archivo = f"{ruta}{row['Platform']}\\{row['Year']}\\archivo.txt"
+            cadena = ''
+
+            for key, value in row.items():
+                cadena += value + ","
+
+            cadena += "\n"
+
+            with open(ruta_archivo, 'a') as archivo:
+                archivo.write(cadena)
+
+end = time.time()
+
+print(end - start)
 # id | Rank | Name | Platform | Year | Genre | Publisher | NA_Sales | EU_Sales | JP_Sales | Other_Sales
 
 # Estructura de directorios
