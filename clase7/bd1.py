77b68092ae510bc1098f9ab958587b6d8dc07c72
Gustavo <desarrollo@healonsite.mx>
2022-05-25 20:37:08 -0500

    conexion y obtener dato postgresql

---
+++ clase7/bd1.py
@@ -0,0 +1,9 @@
+# ejemplo de conexion a base de datos postgres
+# utilizando psycopg2
+import psycopg2
+
+conn = psycopg2.connect("dbname=python_intermedio_db user=postgres password=root")
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM usuarios;")
+print(cur.fetchone())
